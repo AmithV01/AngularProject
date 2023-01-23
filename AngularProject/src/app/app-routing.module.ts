50d7258dced8d0d5f0adf216b8d0a8bf00813158
Amith V <amithv49@gmail.com>
2023-01-23 12:03:57 +0530

    Components added

--- AngularProject/src/app/app-routing.module.ts
+++ AngularProject/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './Components/index/index.component';
+import { HomeComponent } from './Components/LandingPages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "index", component: IndexComponent },
+  { path: "**", component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './Components/index/index.component';
+import { HomeNavBarComponent } from './Components/Navigation/home-nav-bar/home-nav-bar.component';
+import { HomeComponent } from './Components/LandingPages/home/home.component';
+import { AdminComponent } from './Components/LandingPages/admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestnavComponent } from './Components/Navigation/testnav/testnav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeNavBarComponent,
+    HomeComponent,
+    AdminComponent,
+    TestnavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
